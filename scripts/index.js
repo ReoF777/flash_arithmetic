7fb8bbad9a93269a1f34638454f9ca585cd9d827
RFujinami <reonafuji@gmail.com>
2023-08-26 21:10:25 +0900

    フラッシュ暗算の機能を実装

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 let ans = 0;
 
 
-document.getElementById("ansBtn").addEventListener("click", function () {
-    console.log("ansBtn clicked");
-    const ansBtn = document.getElementById("ansBtn");
+document.getElementById("startBtn").addEventListener("click", function () {
+    console.log("startBtn clicked");
     const dispNum = document.getElementById("dispNum");
     const inputNum = document.getElementById("inputNum");
+    ans = 0;
 
-    //inputNum.value = 0;
+    inputNum.value = "";
 
     var spanedSec = 0;
 
@@ -16,6 +16,7 @@ document.getElementById("ansBtn").addEventListener("click", function () {
         var randNum = Math.floor(Math.random() * 10);
 
         if (spanedSec > 5) {
+            dispNum.innerHTML = "";
             clearInterval(id);
         }
 
@@ -24,7 +25,17 @@ document.getElementById("ansBtn").addEventListener("click", function () {
         dispNum.innerHTML = `${randNum}`;
     }, 700);
 
-    dispNum.innerHTML = null;
+    dispNum.innerHTML = "";
+});
 
+document.getElementById("ansBtn").addEventListener("click", function () {
+    console.log("ansBtn clicked");
+    const inputNum = document.getElementById("inputNum");
+    const result = document.getElementById("result");
 
+    if (inputNum.value == ans) {
+        result.innerHTML = "Correct!";
+    } else {
+        result.innerHTML = "Wrong!";
+    }
 });
