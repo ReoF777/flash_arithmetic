5c122830c4453cf0b7d76b450662a18e82892ec2
RFujinami <reonafuji@gmail.com>
2023-08-26 21:21:42 +0900

    フラッシュ暗算の最後の数字が変更されないバグを修正

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,33 @@
 let ans = 0;
 
-
+// スタートボタンを押したら、ランダムな数字を表示するメソッド
 document.getElementById("startBtn").addEventListener("click", function () {
     console.log("startBtn clicked");
     const dispNum = document.getElementById("dispNum");
     const inputNum = document.getElementById("inputNum");
-    ans = 0;
+    let ans = 0;
 
     inputNum.value = "";
 
-    var spanedSec = 0;
+    let spanedSec = 0;
 
-    var id = setInterval(function () {
+    const id = setInterval(function () {
         spanedSec++;
-        var randNum = Math.floor(Math.random() * 10);
 
         if (spanedSec > 5) {
-            dispNum.innerHTML = "";
             clearInterval(id);
+            dispNum.innerHTML = "終了";
+            return;
         }
 
+        const randNum = Math.floor(Math.random() * 10);
         ans += randNum;
 
         dispNum.innerHTML = `${randNum}`;
     }, 700);
-
-    dispNum.innerHTML = "";
 });
 
+// 答え合わせボタンを押したら、入力された数字と答えを比較するメソッド
 document.getElementById("ansBtn").addEventListener("click", function () {
     console.log("ansBtn clicked");
     const inputNum = document.getElementById("inputNum");
